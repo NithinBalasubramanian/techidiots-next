de7adbebf0088713f32d02a0cb4a9dd9cc1f12fe
Nithin <nithinmigocrogle22@outlook.com>
2021-04-17 17:36:42 +0530

    Update home.js

--- component/home.js
+++ component/home.js
@@ -85,9 +85,10 @@ const Home = () => {
             <title>Techidiots - The collection of Tech for Techies</title>
             <meta name="description" content="Techidiots is developed to be a platform to collect latest techology informations from trustable sources and analyse it to present before you."/>
             <link rel="canonical" href="https://techidiots.in" />
+            <link rel="icon" href="/favicon.ico"/>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
             <meta charset="utf-8" />
-            <meta property="og:image" content="/favicon.jpg"/>
+            <meta property="og:image" content="/techidiots.png"/>
             <meta property="og:url" content="https://techidiots.in"/>
             <meta property="og:site_name" content="www.techidiots.in"/>
             <meta property="og:type" content="website" />
@@ -96,7 +97,7 @@ const Home = () => {
 
             <meta property="twitter:url" content="https://techidiots.in/" />
             <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:image" content="https://techidiots.in/favicon.jpg" />
+            <meta property="twitter:image" content="https://techidiots.in/techidiots.png" />
             <meta property="twitter:title" content="TechIdiots - The Collections of Tech for Techies" />
             <meta property="twitter:description" content="Techidiots is developed to be a platform to collect latest techology informations from trustable sources and analyse it to present before you." />
             <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
@@ -106,6 +107,15 @@ const Home = () => {
             <meta name="publisher" content="Techidiots - Pingifinit"/>
             <meta name="owner" content="Techidiots - Pingifinit"/>
             <meta name="keywords" content="Techidiots,technews,latest technology, technology,Marketing , tesla , Programming ,Hyundai ,Jaguar ,Facebook ,Cybersecurity ,Remote ,Cloud crypto,bitcoin,Microsoft ,google,apple,blockchain,Ethereum ,youtube , Nithin balasubramanian , rajan karan ,AlphaFold , DeepMind , Artificial Intelligence trends , Martech"/>
+            <meta name="facebook-domain-verification" content="q5r5o0qa0x8ohoxlhfp1xf5itrgkm7" />
+                    
+            <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+            <link rel="manifest" href="/site.webmanifest">
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+            <meta name="msapplication-TileColor" content="#da532c">
+                
         </Head>
         <div className="contMain">
             <div className="homeListContainer">
