984d60d80b402218d9a598ec7ec1bf589374a57a
Nithin <nithinmigocrogle22@outlook.com>
2021-06-12 16:10:56 +0530

    Update [blogCont].js

--- pages/Blog/[blogHead]/[blogCont].js
+++ pages/Blog/[blogHead]/[blogCont].js
@@ -200,16 +200,7 @@ const BlogHead = () => {
                                     : null
                                     }
                                     <p className="reference"> Reference : { itm.reference } </p>
-                                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                        <ins class="adsbygoogle"
-                                             style="display:block; text-align:center;"
-                                             data-ad-layout="in-article"
-                                             data-ad-format="fluid"
-                                             data-ad-client="ca-pub-3827320441512963"
-                                             data-ad-slot="1365552712"></ins>
-                                        <script>
-                                             (adsbygoogle = window.adsbygoogle || []).push({});
-                                        </script>
+                                      
                               </div>
                             </div>
                             </div>
