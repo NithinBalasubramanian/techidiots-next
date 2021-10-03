4ffd5e7dee653c8abac32833304985492e2436ff
Nithin <nithinfurie17@gmail.com>
2021-10-03 19:59:38 +0530

    PWA Setup

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
